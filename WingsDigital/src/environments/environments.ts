ce5e1606ccad10de3f0dfc909ae970e2ef2ee7a8
xPeenPapi <156064701+xPeenPapi@users.noreply.github.com>
2026-01-11 15:06:11 -0700

    environmentttt

--- WingsDigital/src/environments/environments.ts
+++ WingsDigital/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://thriving-emotion-production-1e53.up.railway.app'
+  apiUrl: 'https://thriving-emotion-production-1e53.up.railway.app/api'
 };
